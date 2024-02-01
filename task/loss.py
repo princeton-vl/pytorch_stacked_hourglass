a7588ef1343074fc6926d1f6f4c45d55c6ddda84
ewernn <eawern@outlook.com>
2024-01-31 20:08:40 -0800

    testing funky latloss3

--- task/loss.py
+++ task/loss.py
@@ -1,13 +1,14 @@
 import torch
 import os
 
+# for a single stack; pred.shape/gt.shape: [4, 6, 75, 75]
 class HeatmapLoss(torch.nn.Module):
     def __init__(self, extra_weight=0.2):
         super(HeatmapLoss, self).__init__()
         self.extra_weight = extra_weight
 
     def forward(self, pred, gt):
-        basic_loss = ((pred - gt)**2).mean(dim=3).mean(dim=2).mean(dim=1)  # shape: [batch_size, nstack]
+        basic_loss = ((pred - gt)**2).mean(dim=3).mean(dim=2).mean(dim=1)  # shape: [batch_size]
         lateral_pred = pred[:, :2, :, -1]
         lateral_gt = gt[:, :2, :, -1]
         focused_loss = ((lateral_pred - lateral_gt)**2).mean(dim=2).mean(dim=1) * self.extra_weight
